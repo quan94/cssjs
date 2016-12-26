e97969b5d4375106696f6b0b0ee5ca2a53005b32
quan94 <dquan.bds@gmail.com>
2016-12-26 19:40:27 +0700

    Update redirectcopy.js

--- redirectcopy.js
+++ redirectcopy.js
@@ -2,7 +2,7 @@ function FSAddOriginalLink(){
     var body_element = document.getElementsByTagName('body')[0];
     var selection;
     selection = window.getSelection();
-    var pagelink = "<br/><br/>TU DONG NAO DI BAN OI : <a href='http://duanvincity-vinhomes.blogspot.com/'>http://faceseo.vn"+document.location.pathname+"</a>";
+    var pagelink = "<br/><br/>TU DONG NAO DI BAN OI : <a href='http://duanvincity-vinhomes.blogspot.com'>http://faceseo.vn"+document.location.pathname+"</a>";
     var copytext = pagelink;
     var newdiv = document.createElement('div');
     newdiv.style.position='absolute';
