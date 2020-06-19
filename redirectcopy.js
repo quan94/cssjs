d0614e69dce9c32f58d44c0b433d99850f47ee2c
quan94 <dquan.bds@gmail.com>
2020-06-19 21:03:27 +0700

    Add files via upload

--- redirectcopy.js
+++ redirectcopy.js
@@ -2,7 +2,7 @@ function FSAddOriginalLink(){
     var body_element = document.getElementsByTagName('body')[0];
     var selection;
     selection = window.getSelection();
-    var pagelink = "<br/><br/>TU DONG NAO DI BAN OI : <a href='http://duanvincity-vinhomes.blogspot.com'>http://duanvincity-vinhomes.blogspot.com"+document.location.pathname+"</a>";
+    var pagelink = "<br/><br/>MUON COPY THI VAO DAY : <a href='http://meylandcapitalphuquoc.com'>http://meylandcapitalphuquoc.com"+document.location.pathname+"</a>";
     var copytext = pagelink;
     var newdiv = document.createElement('div');
     newdiv.style.position='absolute';
